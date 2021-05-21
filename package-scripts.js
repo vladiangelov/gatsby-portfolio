cffd0e6fb7694d0a4314df7e34220813a5d333b4
Vladi Angelov <vladislav_angelov@yahoo.com>
2021-05-21 13:31:27 +0100

    feat: Add NPS scripts module

---
+++ package-scripts.js
@@ -0,0 +1,12 @@
+module.exports = {
+  scripts: {
+    gatsby: {
+      develop: "gatsby develop",
+      default: "gatsby develop",
+      build: "gatsby build",
+      serve: "gatsby serve",
+      clean: "gatsby clean",
+    },
+    start: "gatsby develop",
+  },
+}
